da5f35310ebbcf6a0bc4b924547c3a6ffe47bcae
Cristian Sancricca <cristiansancricca@gmail.com>
2022-06-25 10:10:12 -0300

    delete main storyboard

---
+++ ForecastApp/WeatherViewController.swift
@@ -0,0 +1,42 @@
+//
+//  WeatherViewController.swift
+//  WeatherApp
+//
+//  Created by Cristian Sancricca on 25/06/2022.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    //MARK: - Properties
+    
+//    lazy var viewModel: WeatherViewModel = {
+//        let weatherViewModel = WeatherViewModel()
+//        weatherViewModel.delegate = self
+//        return weatherViewModel
+//    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+        //viewModel.getWeatherData(cityName: "london")
+    }
+    
+
+   
+
+}
+
+
+//MARK: - Weather Delegates
+
+//extension WeatherViewController: WeatherViewModelDelegate {
+//    func didGetWeatherData(weather: WeatherModel) {
+//        print(weather.temperature)
+//    }
+//    func didFailGettingWeatherData(error: String) {
+//        print(error)
+//    }
+//}
